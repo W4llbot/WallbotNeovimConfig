06a131dd860b43ef4e7594a65183cf04b1b7db31
Andy <cahchenanhong@gmail.com>
2026-04-01 00:45:01 +0800

    improved telescope with fs fc fi

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_user_command(
+  'TelescopeConfig',
+  function()
+    require('telescope.builtin').find_files({
+      cwd = vim.fn.stdpath('config')
+    })
+  end,
+  { desc = 'Find files in Neovim config directory' }
+)
+
 return {
     'nvim-telescope/telescope.nvim', version = '*',
     dependencies = {
@@ -11,5 +21,13 @@ return {
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	vim.keymap.set('n', '<leader>fi', builtin.git_files, { desc = 'Telescope find git files' })
+	vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope find symbols' })
+	vim.keymap.set('n', '<leader>fi', builtin.git_files, { desc = 'Telescope find git files' })
+	vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
+
+        vim.keymap.set('n', '<leader>fc', function()
+                builtin.find_files({ cwd = vim.fn.stdpath('config') })
+        end, { desc = 'Telescope find config files' })
     end
 }
