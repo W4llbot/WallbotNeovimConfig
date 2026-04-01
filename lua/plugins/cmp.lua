d1d99d02e9ceb63af685ebfea54396ecb3f77db1
Andy <cahchenanhong@gmail.com>
2026-04-01 16:36:11 +0800

    refactored cmp into own file

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,40 @@
+return {
+        "saghen/blink.cmp",
+        version = "*", -- Use a release tag to download pre-built binaries
+        opts = {
+                keymap = { preset = 'default' }, -- Sets default keymaps for completion
+                appearance = {
+                        use_nvim_cmp_as_default = true, -- Fallback for icons/highlights
+                        nerd_font_variant = 'mono'
+                },
+                sources = {
+                        default = { 'lsp', 'path', 'snippets', 'buffer' },
+                },
+                signature = {
+                        enabled = true,
+                        trigger = {
+                                show_on_trigger_character = true,
+                                show_on_insert_on_trigger_character = true,
+                                show_on_keyword = true,
+                        },
+                        window = {
+                                show_documentation = true,
+                        }
+                },
+                completion = {
+                        documentation = {
+                                auto_show = true,          -- Show documentation for the selected item automatically
+                                --auto_show_delay_ms = 200,   -- Delay before showing the window
+                        },
+                },
+                --                     keymap = {
+                        --                             -- Keybind to manually toggle the documentation window
+                        --                             ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                        --                             -- Keybinds to scroll the documentation window
+                        --                             ["<C-up>"] = { "scroll_documentation_up", "fallback" },
+                        --                             ["<C-down>"] = { "scroll_documentation_down", "fallback" },
+                        --                     },
+        },
+        opts_extend = { "sources.default" }
+}
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,83 +6,42 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 return {
-        {
-                "neovim/nvim-lspconfig",
-                dependencies = {
-                        { "williamboman/mason.nvim", config = true }, -- Mason must run before lspconfig
-                        "williamboman/mason-lspconfig.nvim",
-                        "saghen/blink.cmp", -- Dependency to ensure capabilities are available
-                },
-                config = function()
-                        local lspconfig = require("lspconfig")
-                        local mason_lspconfig = require("mason-lspconfig")
-
-                        -- Get blink.cmp capabilities to pass to LSP servers
-                        local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-                        mason_lspconfig.setup({
-                                ensure_installed = {
-                                        "lua_ls",
-                                        "ts_ls",
-                                        "clangd",
-                                        "pyright",
-                                }, -- Add your servers here
-                                handlers = {
-                                        -- Default handler for installed servers
-                                        function(server_name)
-                                                lspconfig[server_name].setup({
-                                                        capabilities = capabilities,
-                                                })
-                                        end,
-                                        -- Dedicated handlers for specific servers (optional)
---                                        ["lua_ls"] = function()
---                                                lspconfig.lua_ls.setup({
---                                                        capabilities = capabilities,
---                                                        settings = { Lua = { diagnostics = { globals = { "vim" } } } }
---                                                })
---                                        end,
-                                },
-                        })
-                end,
+        "neovim/nvim-lspconfig",
+        dependencies = {
+                { "williamboman/mason.nvim", config = true }, -- Mason must run before lspconfig
+                "williamboman/mason-lspconfig.nvim",
+                "saghen/blink.cmp", -- Dependency to ensure capabilities are available
         },
+        config = function()
+                local lspconfig = require("lspconfig")
+                local mason_lspconfig = require("mason-lspconfig")
 
-        {
-                "saghen/blink.cmp",
-                version = "*", -- Use a release tag to download pre-built binaries
-                opts = {
-                        keymap = { preset = 'default' }, -- Sets default keymaps for completion
-                        appearance = {
-                                use_nvim_cmp_as_default = true, -- Fallback for icons/highlights
-                                nerd_font_variant = 'mono'
-                        },
-                        sources = {
-                                default = { 'lsp', 'path', 'snippets', 'buffer' },
-                        },
-                        signature = {
-                                enabled = true,
-                                trigger = {
-                                        show_on_trigger_character = true,
-                                        show_on_insert_on_trigger_character = true,
-                                        show_on_keyword = true,
-                                },
-                                window = {
-                                        show_documentation = true,
-                                }
-                        },
-                        completion = {
-                                documentation = {
-                                        auto_show = true,          -- Show documentation for the selected item automatically
-                                        --auto_show_delay_ms = 200,   -- Delay before showing the window
-                                },
-                        },
-   --                     keymap = {
-   --                             -- Keybind to manually toggle the documentation window
-   --                             ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-   --                             -- Keybinds to scroll the documentation window
-   --                             ["<C-up>"] = { "scroll_documentation_up", "fallback" },
-   --                             ["<C-down>"] = { "scroll_documentation_down", "fallback" },
-   --                     },
-                },
-                opts_extend = { "sources.default" }
-        },
-}
+                -- Get blink.cmp capabilities to pass to LSP servers
+                local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+                mason_lspconfig.setup({
+                        ensure_installed = {
+                                "lua_ls",
+                                "ts_ls",
+                                "clangd",
+                                "pyright",
+                        }, -- Add your servers here
+                        handlers = {
+                                -- Default handler for installed servers
+                                function(server_name)
+                                        lspconfig[server_name].setup({
+                                                capabilities = capabilities,
+                                        })
+                                end,
+                                -- Dedicated handlers for specific servers (optional)
+                                --                                        ["lua_ls"] = function()
+                                        --                                                lspconfig.lua_ls.setup({
+                                                --                                                        capabilities = capabilities,
+                                                --                                                        settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+                                                --                                                })
+                                                --                                        end,
+                                        },
+                                })
+                        end,
+
+                }
